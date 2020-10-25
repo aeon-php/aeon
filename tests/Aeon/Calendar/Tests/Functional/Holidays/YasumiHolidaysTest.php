1fce59d212cb03ecca3faa4905c9f89a6db6b5aa
Norbert Orzechowicz <norbert@orzechowicz.pl>
2020-10-25 09:32:38 +0100

    Updated dependencies

--- tests/Aeon/Calendar/Tests/Functional/Holidays/YasumiHolidaysTest.php
+++ tests/Aeon/Calendar/Tests/Functional/Holidays/YasumiHolidaysTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Aeon\Calendar\Tests\Functional\Holidays;
 
+use Aeon\Calendar\Gregorian\DateTime;
 use Aeon\Calendar\Gregorian\Day;
 use Aeon\Calendar\Gregorian\GregorianCalendarStub;
+use Aeon\Calendar\Gregorian\TimeZone;
 use Aeon\Calendar\Gregorian\YasumiHolidays;
 use PHPUnit\Framework\TestCase;
 use Yasumi\Provider\Poland;
@@ -14,7 +16,9 @@ final class YasumiHolidaysTest extends TestCase
 {
     public function test_yasumi_holidays() : void
     {
-        $holidays = YasumiHolidays::provider(Poland::class, new GregorianCalendarStub(new \DateTimeImmutable('2020-01-01')));
+        $calendar = new GregorianCalendarStub(TimeZone::UTC());
+        $calendar->setNow(DateTime::fromString('2020-01-01'));
+        $holidays = YasumiHolidays::provider(Poland::class, $calendar);
 
         $this->assertTrue($holidays->isHoliday(Day::fromString('2020-01-01')));
         $this->assertCount(1, $holidays->holidaysAt(Day::fromString('2020-01-01')));
@@ -24,6 +28,6 @@ final class YasumiHolidaysTest extends TestCase
     {
         $this->expectExceptionMessage('non_provider is not valid Yasumi provider class.');
 
-        YasumiHolidays::provider('non_provider', new GregorianCalendarStub(new \DateTimeImmutable('2020-01-01')));
+        YasumiHolidays::provider('non_provider', new GregorianCalendarStub(TimeZone::UTC()));
     }
 }
